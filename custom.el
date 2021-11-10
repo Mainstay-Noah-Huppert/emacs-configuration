dacfd5b4ccaf1ff7e9ff32151b409209c90a4e3d
Noah Huppert <noahhuppert@admithub.com>
2021-11-10 17:00:44 -0500

    customize to try and fix typescript switch statement indent
    I don't think it worked :/

--- custom.el
+++ custom.el
@@ -9,7 +9,8 @@
  '(image-dired-append-when-browsing t)
  '(image-dired-thumb-size 100)
  '(projectile-mode t nil (projectile))
- '(size-indication-mode t))
+ '(size-indication-mode t)
+ '(typescript-indent-switch-clauses t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
