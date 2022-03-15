7942dae31be4cd08d3d813bedc9956d4eacb4a54
Noah Huppert <noah@mainstay.com>
2022-03-15 19:50:47 -0400

    metis: added LaTeX bin to path

--- machines/metis.el
+++ machines/metis.el
@@ -1,7 +1,8 @@
 ;; Set custom path locations
-(setenv "PATH" (concat (getenv "PATH") ":" "/usr/local/bin"))
+(setenv "PATH" (concat (getenv "PATH") ":" "/usr/local/bin:/Library/TeX/texbin/"))
 (setq ispell-program-name "/usr/local/bin/ispell")
 (add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "/Library/TeX/texbin/")
 
 ;; Location of Org mode agenda files on this machine
 (setq my-org-agenda-files
