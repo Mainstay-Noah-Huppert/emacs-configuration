5b4d25ca295fa80550620a6eed4bf2c4b04f0eee
Noah Huppert <noah@mainstay.com>
2022-07-13 10:26:34 -0400

    metis: configured sshx tramp w coder

--- machines/metis-post.el
+++ machines/metis-post.el
@@ -2,3 +2,7 @@
 (tramp-set-completion-function "ssh"
 						 '((tramp-parse-sconfig "~/.ssh/config")
 						   (tramp-parse-sconfig "~/.ssh/coder")))
+
+(tramp-set-completion-function "sshx"
+						 '((tramp-parse-sconfig "~/.ssh/config")
+						   (tramp-parse-sconfig "~/.ssh/coder")))
