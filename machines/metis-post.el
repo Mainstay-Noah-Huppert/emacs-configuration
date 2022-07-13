88fc1c03ba6daa7f2f16336a6bf2a9de74cbe1c4
Noah Huppert <noah@mainstay.com>
2022-07-13 10:26:49 -0400

    metis: trying to speed up tramp

--- machines/metis-post.el
+++ machines/metis-post.el
@@ -6,3 +6,12 @@
 (tramp-set-completion-function "sshx"
 						 '((tramp-parse-sconfig "~/.ssh/config")
 						   (tramp-parse-sconfig "~/.ssh/coder")))
+
+(setq remote-file-name-inhibit-cache nil)
+(setq vc-ignore-dir-regexp
+      (format "%s\\|%s"
+                    vc-ignore-dir-regexp
+                    tramp-file-name-regexp))
+(setq tramp-verbose 10)
+
+
